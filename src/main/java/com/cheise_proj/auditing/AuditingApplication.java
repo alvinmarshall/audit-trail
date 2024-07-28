93dfadee97ff28ba257edc7b5869122f1fece651
alvinmarshall <kelvinbirikorang@gmail.com>
2024-07-28 12:22:51 -0400

    chore: add jpa AuditAwareImpl

--- src/main/java/com/cheise_proj/auditing/AuditingApplication.java
+++ src/main/java/com/cheise_proj/auditing/AuditingApplication.java
@@ -2,10 +2,8 @@ package com.cheise_proj.auditing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class AuditingApplication {
 
     public static void main(String[] args) {
